47e6ca1cffa10a210f5c273bac043510d4315944
J-oss-e <josesilvaangelmunoz@gmail.com>
2026-02-02 19:55:55 -0600

    Finished the clock classwork

--- 1st Classwork/src/Clock.java
+++ 1st Classwork/src/Clock.java
@@ -12,12 +12,6 @@ public class Clock {
         int centerX = image.getWidth()/2, centerY = image.getHeight()/2;
         int radius = 100;
 
-        for(int x = 0; x < image.getWidth(); x++){
-            for(int y = 0; y < image.getHeight(); y++){
-
-            }
-        }
-
         for(int x = 0; x < image.getWidth(); x ++) {
             for(int y = 0; y < image.getHeight(); y++) {
 
@@ -29,6 +23,39 @@ public class Clock {
                     image.setRGB(x, y, Color.white.getRGB());
                 }
             }
+        }
+
+        for (int i = 0; i < 12; i++) {
+            double angulo = i * (Math.PI * 2 / 12);
+
+
+            int puntoX = (int) (centerX + (radius - 10) * Math.cos(angulo));
+            int puntoY = (int) (centerY + (radius - 10) * Math.sin(angulo));
+
+            // Pintamos un pequeño punto (un cuadrado de 3x3 pixeles)
+            for(int x = -1; x <= 1; x++){
+                for(int y = -1; y <= 1; y++){
+                    image.setRGB(puntoX + x, puntoY + y, Color.white.getRGB());
+                }
+            }
+        }
+
+        double anguloHora = 3.6;
+        int largoHora = 50;
+
+        for (int r = 0; r < largoHora; r++) {
+            int x = (int) (centerX + r * Math.cos(anguloHora));
+            int y = (int) (centerY + r * Math.sin(anguloHora));
+            image.setRGB(x, y, Color.white.getRGB());
+        }
+
+        double anguloMinuto = 5.2;
+        int largoMinuto = 80;
+
+        for (int r = 0; r < largoMinuto; r++) {
+            int x = (int) (centerX + r * Math.cos(anguloMinuto));
+            int y = (int) (centerY + r * Math.sin(anguloMinuto));
+            image.setRGB(x, y, Color.white.getRGB());
         }
 
         File outputfile = new File("clock.jpg");
