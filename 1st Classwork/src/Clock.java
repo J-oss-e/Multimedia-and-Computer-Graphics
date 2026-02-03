d3ebac27d5b1d4aa0993abfad6fbd712cf64318e
angel <sistemasaycbecarios@gmail.com>
2026-02-02 18:55:27 -0600

    Advances in clock.java classwork

--- 1st Classwork/src/Clock.java
+++ 1st Classwork/src/Clock.java
@@ -2,14 +2,34 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import static java.lang.Math.*;
 import java.awt.image.BufferedImage;
 
 public class Clock {
     public static void main(String[] args) {
-        BufferedImage image = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(600, 400, BufferedImage.TYPE_INT_RGB);
 
+        int centerX = image.getWidth()/2, centerY = image.getHeight()/2;
+        int radius = 100;
 
+        for(int x = 0; x < image.getWidth(); x++){
+            for(int y = 0; y < image.getHeight(); y++){
 
+            }
+        }
+
+        for(int x = 0; x < image.getWidth(); x ++) {
+            for(int y = 0; y < image.getHeight(); y++) {
+
+                double distanciaX = x - centerX;
+                double distanciaY = y - centerY;
+                double distanciaTotal = sqrt((distanciaY*distanciaY) + (distanciaX*distanciaX));
+
+                if (distanciaTotal > radius - 1 && distanciaTotal < radius + 1) {
+                    image.setRGB(x, y, Color.white.getRGB());
+                }
+            }
+        }
 
         File outputfile = new File("clock.jpg");
         try {
